ecf270a6008cf6059a5e5961640c86284fe2daf2
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2017-04-27 11:49:35 +0200

    excluding all classes setup

--- train_oneshot.py
+++ train_oneshot.py
@@ -7,11 +7,13 @@ import sys
 import time
 import os
 
+
 import augmentation as aug
 import convnet_v2_oneshot as cnn
 #import convnet as cnn
 import load_class
 from util.dataprocessing import DataSaver
+from sklearn import metrics
 
 augmenter = aug.augmenter()
 loader = load_class.load(15)
@@ -23,7 +25,7 @@ x_test, labels_test, indices_test = loader.load_testing_set()
 
 class_accuracies = np.zeros(20,dtype=np.int)
 
-convnet = cnn.convnet_oneshot(num_output_units=20, num_layers_retrain=1)
+# convnet = cnn.convnet_oneshot(num_output_units=20, num_layers_retrain=1)
 
 
 base_dir_path = "{}/".format(os.path.dirname(os.path.abspath(__file__))) #"/home/jasper/oneshot-gestures/
@@ -77,35 +79,40 @@ def getClassAccuracy(targets, predictions, class_num):
                 predict_count += 1
     return predict_count, class_count
 
-def validate():
+def validate(convnet):
     val_err = 0
     val_acc = 0
     val_batches = 0
-    num_valid_class_acc = 0
-    class_acc = 0
+    precision_score = np.zeros((num_classes))
+    recall_score = np.zeros((num_classes))
+    # num_valid_class_acc = 0
+    # class_acc = 0
     for batch in iterate_minibatches(x_validate, labels_validate, batch_size, indices_validate, True):
         inputs, targets = batch
         err, acc = convnet.validate(inputs, targets)
         val_err += err
         val_acc += acc
-        predict_count, class_count = getClassAccuracy(targets, convnet.test_output(inputs), oneshot_class)
-        if ( class_count != 0 ):
-            class_acc += 1.0 * predict_count / class_count
-            num_valid_class_acc += 1
-        val_batches += 1
-    return val_err/val_batches, val_acc/val_batches, class_acc/num_valid_class_acc
+        precision_score = np.add(precision_score,metrics.precision_score(targets,convnet.test_output(inputs),xrange(num_classes),average=None))
+        recall_score    = np.add(recall_score, metrics.recall_score(targets,convnet.test_output(inputs),xrange(num_classes),average=None))
 
-def test():
-    test_err = 0
-    test_acc = 0
-    test_batches = 0
-    for batch in iterate_minibatches(x_test, labels_test, batch_size, indices_test, shuffle=False):
-        inputs, targets = batch
-        err, acc = convnet.validate(inputs, targets)
-        test_err += err
-        test_acc += acc
-        test_batches += 1
-    return test_acc / test_batches
+        # predict_count, class_count = getClassAccuracy(targets, convnet.test_output(inputs), oneshot_class)
+        # if ( class_count != 0 ):
+        #     class_acc += 1.0 * predict_count / class_count
+        #     num_valid_class_acc += 1
+        val_batches += 1
+    return val_err/val_batches, val_acc/val_batches, precision_score/val_batches, recall_score/val_batches
+
+# def test(convnet):
+#     test_err = 0
+#     test_acc = 0
+#     test_batches = 0
+#     for batch in iterate_minibatches(x_test, labels_test, batch_size, indices_test, shuffle=False):
+#         inputs, targets = batch
+#         err, acc = convnet.validate(inputs, targets)
+#         test_err += err
+#         test_acc += acc
+#         test_batches += 1
+#     return test_acc / test_batches
 
 if __name__=='__main__':
     try:
@@ -125,9 +132,10 @@ if __name__=='__main__':
         # retrain_layers = 3
         # for num_oneshot_samples in [200,100,50,25,10]:
         # num_oneshot_samples = 2
-        for num_oneshot_samples in [200]:
+        for num_oneshot_samples in [1,2,5,10,25]:
             for retrain_layers in [1]:
-                ds = DataSaver(('train_loss', 'val_loss', 'val_acc', 'class_acc', 'dt'))
+                ds = DataSaver(('train_loss', 'val_loss', 'class_acc', 'dt'))
+
 
                 convnet = cnn.convnet_oneshot(num_output_units=20, num_layers_retrain=retrain_layers)
                 indices_train[oneshot_class] = indices_train[oneshot_class][:num_oneshot_samples]
@@ -145,6 +153,8 @@ if __name__=='__main__':
                 try:
                     backprops_per_epoch = 200
                     num_backprops = 20000 / backprops_per_epoch
+                    precision_list = np.zeros((num_backprops,num_classes))
+                    recall_list = np.zeros((num_backprops,num_classes))
                     for j in xrange(num_backprops):
                         #Initialise new random permutation of data
                         #And load first batch of augmented samples
@@ -171,32 +181,39 @@ if __name__=='__main__':
 
                             q.join()
                         train_loss = train_err / backprops_per_epoch
-                        val_loss, val_acc, class_acc = validate()
+                        val_loss, val_acc, precision_score, recall_score = validate(convnet)
+                        precision_list[j] = precision_score
+                        recall_list[j] = recall_score
+
 
                         if (val_loss < min_val_err):
                             min_val_err = val_loss
                             convnet.save_param_values(save_param_path)
 
-                        print("\nval err:\t{:5.2f}\nval acc:\t{:5.2f}%\nclass acc:\t{:5.2f}%"
-                              .format(val_loss,val_acc * 100.0,class_acc * 100.0))
-                        ds.saveValues((train_loss,val_loss,val_acc,class_acc,time.time()-start_time))
+                        print("\nval err:\t{:5.2f}\nval acc:\t{:5.2f}%\nprecision:\t{:5.2f}%\nrecall:\t{:5.2f}%"
+                              .format(val_loss,val_acc * 100.0,precision_score[num_classes-1] * 100.0,recall_score[num_classes-1] * 100.0))
+                        ds.saveValues((train_loss,val_loss,val_acc,time.time()-start_time))
 
                 except KeyboardInterrupt:
                     print("Iteration stopped through KeyboardInterrupt")
                 except:
                     raise
                 finally:
-
-                    convnet.load_param_values(save_param_path)
-                    test_acc = test()
-                    print("test-acc:{:5.2f}%".format(test_acc * 100))
+                    if os.path.exists(save_param_path):
+                        convnet.load_param_values(save_param_path)
+                        test_acc, test_acc, precision_score, recall_score = validate(convnet)
+                        print("test-acc:{:5.2f}%".format(test_acc * 100))
+                        print(precision_score)
+                        print(recall_score)
 
                     directory = "{}output/datav2-{}-retrain-{}-samples-{}/".format(base_dir_path, oneshot_class,retrain_layers,num_oneshot_samples)
-                    #directory = "{}output/data-{}/".format(base_dir_path, oneshot_class)
                     if not os.path.exists(directory):
                         os.makedirs(directory)
                     ds.saveToArray(directory)
                     ds.saveToCsv(directory,"acc_loss")
+                    np.save("{}precision".format(directory),precision_list)
+                    np.save("{}recall".format(directory),recall_list)
+
 
     except:
         raise
