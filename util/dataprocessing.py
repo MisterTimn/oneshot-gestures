1c9a98dc367d3333309de3d047201d293c39baf3
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2017-04-27 22:44:35 +0200

    saving y_test for confusion matrix

--- util/dataprocessing.py
+++ util/dataprocessing.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import itertools
+
+from sklearn import metrics
 
 
 class DataSaver:
@@ -86,6 +89,58 @@ class DataPlotter:
         for data in list_of_data:
             plt.plot(data, 'o')
 
+        plt.show()
+
+    def plot_confusion_matrix(cm, classes,
+                              normalize=False,
+                              title='Confusion matrix',
+                              cmap=plt.cm.Blues):
+        """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+        """
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
+
+        print(cm)
+
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, cm[i, j],
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+
+    def plotConfusionMatrix(self, y_test, y_pred):
+        class_names = ("0")
+        for i in xrange(1,20):
+            class_names.append("{}".format(i))
+
+        cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+        np.set_printoptions(precision=2)
+
+        # Plot non-normalized confusion matrix
+        plt.figure()
+        self.plot_confusion_matrix(cnf_matrix, classes=class_names,
+                              title='Confusion matrix, without normalization')
+
+        # Plot normalized confusion matrix
+        plt.figure()
+        self.plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                              title='Normalized confusion matrix')
         plt.show()
 
 
