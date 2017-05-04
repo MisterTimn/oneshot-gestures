9d016e2c02a2fda7706b1bae20448fcb24b05bea
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2017-05-04 23:24:10 +0200

    oneshot double

--- train_oneshot_double.py
+++ train_oneshot_double.py
@@ -117,7 +117,7 @@ if __name__=='__main__':
         # retrain_layers = 3
         # for num_oneshot_samples in [200,100,50,25,10]:
         # num_oneshot_samples = 2
-        for num_oneshot_samples in [1,2,3,4,5]:
+        for num_oneshot_samples in [3,4,5,25,100]:
             for retrain_layers in [1]:
                 ds = DataSaver(('train_loss', 'val_loss', 'val_acc', 'dt'))
                 precision_list = np.zeros((NUM_EPOCHS, NUM_CLASSES))
