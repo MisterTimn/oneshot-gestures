c59e0c4683189ddcdc30e9fe3f21cee7e70a05a4
Jasper Vaneessen <jasper.vaneessen@gmail.com>
2017-05-27 14:56:09 +0200

    oneshot redo

--- train_oneshot_extra_2.py
+++ train_oneshot_extra_2.py
@@ -102,7 +102,7 @@ if __name__=='__main__':
         # retrain_layers = 3
         # for num_oneshot_samples in [200,100,50,25,10]:
         # num_oneshot_samples = 2
-        for ONESHOT_CLASS in xrange(15,20):
+        for ONESHOT_CLASS in xrange(5,10):
 
             OUTPUT_DIRECTORY = "{}output/{}/class-{}/".format(BASE_DIR, MODEL_VERS, ONESHOT_CLASS)
             PARAM_DIRECTORY = "{}convnet_params/{}/class-{}/".format(BASE_DIR, MODEL_VERS, ONESHOT_CLASS)
@@ -125,7 +125,7 @@ if __name__=='__main__':
             proc.daemon = True
             proc.start()
 
-            for num_oneshot_samples in [1]:
+            for num_oneshot_samples in [5]:
                 for retrain_layers in [1]:
                     q.put('change_num_samples')
                     q.join()
